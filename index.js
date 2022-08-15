c5779af0a5e438a9668153efd5ce87ca37c0b020
Lincoln <lincoln@taylorlivingco.com>
2022-08-15 15:22:51 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+
+let driversArr= ['Bobby', 'sunny', 'Ally', 'Annette', 'Sam', 'Bobby'];
+
+function findMatching(driversArr, string){
+    return driversArr.filter ((filtered) => filtered.toLowerCase() === string.toLowerCase()
+    );
+};
+
+console.log(findMatching(driversArr, 'Bobby'));
+
+function fuzzyMatch(driversArr, string){
+    return driversArr.filter ((m) => m.startsWith(string));
+}
+
+console.log(fuzzyMatch(driversArr, 'Mike'));
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+    ];
+
+function matchName(drivers, string){
+    return drivers.filter((x) => x.name === string);
+}
+
+console.log(matchName(drivers, 'Sally'));
